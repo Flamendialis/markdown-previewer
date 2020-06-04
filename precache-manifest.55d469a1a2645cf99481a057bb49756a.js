4ecd701d842bf74718664dbbd61efead71821544
Flamen_Dialis <menendezrfelixj@gmail.com>
2020-06-04 18:24:12 -0300

    Updates

---
+++ precache-manifest.55d469a1a2645cf99481a057bb49756a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7dd8346c73062f2a411fe405e36fb1",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "7b01efd0eb3c606fc9a0",
+    "url": "/markdown-previewer/static/css/main.b98e77f5.chunk.css"
+  },
+  {
+    "revision": "f7c63463aa84addb92b5",
+    "url": "/markdown-previewer/static/js/2.3c6e0a5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.3c6e0a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b01efd0eb3c606fc9a0",
+    "url": "/markdown-previewer/static/js/main.26b142d1.chunk.js"
+  },
+  {
+    "revision": "35deca1419128cdfce54",
+    "url": "/markdown-previewer/static/js/runtime-main.5fc24b94.js"
+  }
+]);
